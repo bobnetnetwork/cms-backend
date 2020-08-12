aa51c321f48df08d58f4da7033c2e85fd0ccb995
bobesz <bob@bobnet.network>
2020-08-12 21:21:48 +0200

    conver dbService to class

--- src/service/dbService.ts
+++ src/service/dbService.ts
@@ -2,41 +2,46 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import {LogService} from "./LogService.js";
 
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config();
-}
+export class DBService {
+    private USER: string = process.env.DB_SERVER_USER;
+    private PWD: string = process.env.DB_SERVER_PWD
+    private TYPE: string = process.env.DB_SERVER_TYPE;
+    private PORT: number = parseInt(process.env.DB_SERVER_PORT as string, 10);
+    private ADDRESS: string = process.env.DB_SERVER_ADDRESS;
+    private DATABASE: string = process.env.DB_SERVER_DATABASE;
 
-const USER: string = process.env.DB_SERVER_USER;
-const PWD: string = process.env.DB_SERVER_PWD
-const TYPE: string = process.env.DB_SERVER_TYPE;
-const PORT: number = parseInt(process.env.DB_SERVER_PORT as string, 10);
-const ADDRESS: string = process.env.DB_SERVER_ADDRESS;
-const DATABASE: string = process.env.DB_SERVER_DATABASE;
+    private log = new LogService().getLogger("dbService");
 
-const log = new LogService().getLogger("dbService");
+    private connection;
+    private readonly dbUri;
 
-let connection;
-const dbUri = "mongodb://" + USER + ":" + PWD + "@" + ADDRESS + ":" + PORT + "/" + DATABASE;
-
-export const connectToDB = () => {
-    mongoose.connect(dbUri, {
-        useNewUrlParser: true,
-        useFindAndModify: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    });
-    connection = mongoose.connection;
-    connection.once("open", async () => {
-        log.info("Connected to database");
-    });
-    connection.on("error", () => {
-        log.error("Error connecting to database");
-    });
-}
-
-export const disconnect = () => {
-    if (!connection) {
-        return;
+    constructor() {
+         if (process.env.NODE_ENV !== 'production') {
+             dotenv.config();
+         }
+         this.dbUri = "mongodb://" + this.USER + ":" + this.PWD + "@" + this.ADDRESS + ":" + this.PORT + "/" + this.DATABASE;
     }
-    mongoose.disconnect();
-};
+
+    public connectToDB() {
+        mongoose.connect(this.dbUri, {
+            useNewUrlParser: true,
+            useFindAndModify: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        this.connection = mongoose.connection;
+        this.connection.once("open", async () => {
+            this.log.info("Connected to database");
+        });
+        this.connection.on("error", () => {
+            this.log.error("Error connecting to database");
+        });
+    }
+
+    public disconnect() {
+        if (!this.connection) {
+            return;
+        }
+        mongoose.disconnect();
+    }
+}
