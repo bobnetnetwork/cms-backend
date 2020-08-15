c2b7c053c2f19d30a0e09b5252eae6e3f57ef6fd
bobesz <bob@bobnet.network>
2020-08-16 00:34:35 +0200

    refactoring

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,4 +1,4 @@
-import HttpException from "../common/http-exception";
+import HttpException from "../common/HttpException";
 import {NextFunction, Request, Response} from "express";
 import {LogService} from "../service/LogService.js";
 
