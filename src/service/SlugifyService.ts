dbcbab48ede6f09d0afcf856760d9f3bb3347154
Bobesz <bob@bobnet.network>
2020-08-17 03:05:01 +0200

    Update SlugifyService.ts

--- src/service/SlugifyService.ts
+++ src/service/SlugifyService.ts
@@ -2,7 +2,7 @@ import slugify from "slugify";
 import fs from "fs";
 
 export class SlugifyService {
-    private readonly conf: string | { replacement?: string; remove?: RegExp; lower?: boolean; strict?: boolean; locale?: string; };
+    private readonly conf: string | { };
 
     constructor() {
         this.conf = JSON.parse(fs.readFileSync("./config/SlugifyConfig.json", "utf-8"));
