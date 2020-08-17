2882aceb3f3cf19d2de09897c1ccfa90ca8d81bc
Bobesz <bob@bobnet.network>
2020-08-17 06:01:41 +0200

    created BaseMessage

--- src/messages/ResultMessage.ts
+++ src/messages/ResultMessage.ts
@@ -1,9 +1,10 @@
-export class ResultMessage {
-    protected readonly message: string;
+import {BaseMessage} from "./BaseMessage.js";
+
+export class ResultMessage extends BaseMessage{
     protected readonly success: boolean;
 
     constructor(message: string, success: boolean) {
-        this.message = message;
+        super(message)
         this.success = success;
     }
 
